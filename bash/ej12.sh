5dfb2d266a5b551ea556c7f21fc454a0cc3ba009
sysadmin <usuario de discor>
2025-04-24 11:39:39 -0300

    Agreg

--- bash/ej12.sh
+++ bash/ej12.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mastermaccontacto@gmail.com
+#chuelmo@gmail.com
 #Ejercicio 12: De un archivo pasado como parámetro, reportar un listado con las 10 palabras más usadas y cuantas veces se ha usado cada una de ellas, ordenando la salida en forma decreciente en función de la cantidad de veces que se han usado (comenzando desde la palabra más usada a las menos usada). 
 
 #Verificar que se recibe un parámetro y que éste es un archivo
