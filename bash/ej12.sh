76322ce64e114b8fb300e2490d064c4ccc770ecf
sysadmin <usuario de discor>
2025-04-24 10:54:13 -0300

    Agregue al comit los ejercicios tuneados

---
+++ bash/ej12.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mastermaccontacto@gmail.com
+#Ejercicio 12: De un archivo pasado como parámetro, reportar un listado con las 10 palabras más usadas y cuantas veces se ha usado cada una de ellas, ordenando la salida en forma decreciente en función de la cantidad de veces que se han usado (comenzando desde la palabra más usada a las menos usada). 
+
+#Verificar que se recibe un parámetro y que éste es un archivo
+
+archivo="$1"
+
+for p in `cat "$archivo"`; do
+	echo "${p,,}"
+done | sort | uniq -c | sort -nr | head -n 10
