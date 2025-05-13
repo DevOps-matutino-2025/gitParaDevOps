be09a0f66cf5c35453255fd00305394185786b49
Giuli <Giuli@devops.com>
2025-05-13 11:25:49 +0000

    Primer ejercicio de Python

---
+++ scripts/ejercicioPosiciones.py
@@ -0,0 +1,14 @@
+def buscarPosiciones(cadena, caracter):
+    
+    posiciones = []
+    
+    for i in range(len(cadena)):
+        if cadena[i] == caracter:
+            posiciones.append(i + 1)
+    return posiciones
+
+palabra = input("Ingrese una cadena: ")
+
+letra = input ("Ingrese un caracter: ")
+
+print(buscarPosiciones(palabra, letra))
